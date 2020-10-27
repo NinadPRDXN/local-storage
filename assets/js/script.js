79ddbc9b0673b5868a015a682bdc43b677731010
NinadPRDXN <ninadp@prdxn.com>
2020-10-28 01:27:56 +0530

    savepoint 2

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,12 +2,19 @@
     Ninad Parkar
 */
 var
+form = document.querySelector('form'), //Form
 item_name = document.querySelector('.item_name'), //Item Name
 item_quantity = document.querySelector('.item_quantity'), //Item Quantity
 save = document.querySelector('.save'), //Save Button
+save_container = document.querySelector('.save_container'), //Save Conatiner
 clear = document.querySelector('.clear'), //Clear Button 
 list = document.querySelector('.list'), //List of Items
-listBody = document.querySelector('.list-body'); //List Body
+listBody = document.querySelector('.list_body'); //List Body
+
+//Blocking form functionality 
+form.onclick = (e) => {
+    e.preventDefault();
+}
 
 //Assigning types of each input field 
 item_name.type = 'text';
@@ -16,35 +23,73 @@ save.type = 'submit';
 clear.type = 'submit';
 
 //Display data of Local Storage
-if(localStorage.hasOwnProperty('users') == false){
-    var 
-    users = [];
-    add(users);
-}
-else {
-    display();
+document.onload = checking();
+
+//Checking whether local storage is empty or not
+function checking() {
+    if(localStorage.hasOwnProperty('users') == false){
+        var 
+        users = [];
+        add(users);
+        list.style.display = 'none';
+    }
+    else {
+        display();
+    }
 }
 
 //Add New Data
-save.onclick = (e) => {
-    e.preventDefault();
+save.onclick = saveData;
+
+function saveData() {
+    checking();
     iname = item_name.value;
     iquantity = item_quantity.value;
     storage = JSON.parse(localStorage['users']);
     storage.push('{"itemName": "' + iname + '","itemQuantity": "' + iquantity + '"}');
     add(storage);
+    form.reset();
     display();
 }
 
 //Edit the Data
-function edit(value) {
-    storage = JSON.parse(localStorage['users']);
-    console.log(JSON.parse(storage[value]));
+function edit(pivot) {
+    var
+    storage = JSON.parse(localStorage['users']),
+    modification = JSON.parse(storage[pivot]),
+    txt = ' ';
+
+    item_name.value = modification.itemName;
+    save_container.innerHTML = '<input type="submit" onclick="modify(' + pivot + ')" value="update">';
+    item_quantity.value = modification.itemQuantity;
+    storage.forEach(listFunction);
+
+    function listFunction(data, index) {
+        if (index == pivot) {}
+        else {
+            data = JSON.parse(storage[index]);
+            txt += '<ul><li>' + data.itemName + '</li><li>' + data.itemQuantity + '</li><li><button onclick="edit(' + index + ')">edit</button></li><li><button onclick="del(' + index + ')">delete</button></li></ul>';
+        }
+    }
+    listBody.innerHTML = txt;
+}
+
+//Update the Data in local storage
+function modify(index) {
+    var iname = item_name.value, iquantity = item_quantity.value, storage = JSON.parse(localStorage['users']);
+    storage.splice(index, 1, '{"itemName": "' + iname + '","itemQuantity": "' + iquantity + '"}');
+    add(storage);
+    form.reset();
+    save_container.innerHTML = '<input type="submit" onclick="saveData()" value="save">';
+    display();
 }
 
 //Delete the Data
-function del(value) {
-    console.log(value);
+function del(index) {
+    var storage = JSON.parse(localStorage['users']);
+    storage.splice(index, 1);
+    add(storage);
+    display();
 }
 
 //Add Data in Storage
@@ -55,9 +100,7 @@ function add(data) {
 
 //Display List
 function display() {
-    var
-    txt = ' ',
-    storage = JSON.parse(localStorage['users']);
+    var txt = ' ', storage = JSON.parse(localStorage['users']);
     storage.forEach(listFunction);
 
     function listFunction(data, index) {
@@ -68,7 +111,12 @@ function display() {
     listBody.innerHTML = txt;
 }
 
-
+//Clear the local Storage
+clear.onclick = () => {
+    localStorage.removeItem('users');
+    form.reset();
+    list.style.display = 'none';
+}
 
 
 
